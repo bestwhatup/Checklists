72ce7e80cf4716db0b89a992ed12e7cf8e7ec20d
Chawatvish Worrapoj <bestwhatup@gmail.com>
2016-02-13 22:02:48 +0700

    add datasource , delegate function about Tableview [Play UITableview]

---
+++ Checklists/ChecklishViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Chawatvish Worrapoj on 2/13/2559 BE.
+//  Copyright © 2559 Chawatvish Worrapoj. All rights reserved.
+//
+
+import UIKit
+
+class ChecklishViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ChecklistItem", forIndexPath: indexPath)
+        
+        let label = tableView.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn iOS Development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+            if cell.accessoryType == .None {
+                cell.accessoryType = .Checkmark
+            } else {
+                cell.accessoryType = .None
+            }
+        }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+
+
+}
+
